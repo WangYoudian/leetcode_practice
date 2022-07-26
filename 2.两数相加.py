1afe27345df9057c0fc66a4ddcc0d9263017489a
WangYoudian <486590722@qq.com>
2022-07-26 23:06:43 +0800

    update 2022年7月26日23:06:38

--- 2.两数相加.py
+++ 2.两数相加.py
@@ -12,5 +12,33 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def teardown(head):
+            arr = []
+            while head:
+                arr.append(head.val)
+                head = head.next
+            res = 0
+            n = len(arr)
+            for i in range(n):
+                res += arr[n - i - 1]
+                if i < n - 1:
+                    res *= 10
+            return res
+        
+        def build(num):
+            res = ListNode()
+            head = ListNode(num % 10)
+            num %= 10
+            res.next = head
+            while num > 0:
+                num %= 10
+                head.next = ListNode(num % 10)
+                head = head.next
+            return res
+        
+        a = teardown(l1)
+        b = teardown(l2)
+        return build(a + b)
+
 # @lc code=end
 
